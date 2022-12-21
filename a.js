42fec292ede8a21c18a75e5b9a0d670f55cf4022
realmojo88 <strikers1999@naver.com>
2022-12-21 20:29:10 +0900

    pann

---
+++ a.js
@@ -0,0 +1,27 @@
+javascript: var head = document.getElementsByTagName("head")[0],
+  aScript = document.createElement("script");
+(aScript.type = "text/javascript"),
+  (aScript.src = "https://html2canvas.hertzen.com/dist/html2canvas.min.js"),
+  head.appendChild(aScript);
+const downloadURI = (t, e) => {
+    let n = document.createElement("a");
+    (n.download = e), (n.href = t), document.body.appendChild(n), n.click();
+  },
+  result = (t, e, n) => {
+    j$("#p-" + e).html(t);
+    let d = t.toDataURL();
+    downloadURI(d, e + ".png");
+  };
+setTimeout(async () => {
+  j$("body").append("<div id='p-title'>title</div>"),
+    j$("body").append("<div id='p-content'>content</div>"),
+    j$("body").append("<div id='p-updown'>updown</div>"),
+    j$("body").append("<div id='p-best'>best</div>"),
+    j$("body").append("<div id='p-comment'>comment</div>"),
+    html2canvas(j$(".pann-title")[0]).then(function (t) {
+      result(t, "title", 1);
+    }),
+    html2canvas(j$(".content")[0]).then(function (t) {
+      result(t, "content", 2);
+    });
+}, 1e3);
--- index.js
+++ index.js
@@ -2,28 +2,30 @@ const twitterClient = require("./twitterClient");
 const { download } = require("./utilities");
 
 const tweet = async () => {
-  const uri = "https://i.imgur.com/Zl2GLjnh.jpg";
-  const filename = "image.png";
+  // const uri = "https://i.imgur.com/Zl2GLjnh.jpg";
+  // const filename = "image.png";
 
-  download(uri, filename, async function () {
-    try {
-      const media_ids = [];
-      for (let i = 0; i < 4; i++) {
-        const mediaId = await twitterClient.v1.uploadMedia("./image.png");
-        media_ids.push(mediaId);
-        console.log(`${i + 1}번째 이미지 업로드 완료`);
-      }
-      await twitterClient.v2.tweet({
-        text: "Hello world! This is an image in Ukraine!",
-        media: {
-          media_ids,
-        },
-      });
-      console.log("done");
-    } catch (e) {
-      console.log(e);
+  const arr = ["title", "content"];
+
+  // download(uri, filename, async function () {
+  try {
+    const media_ids = [];
+    for (let i = 0; i < arr.length; i++) {
+      const mediaId = await twitterClient.v1.uploadMedia(`./${arr[i]}.png`);
+      media_ids.push(mediaId);
+      console.log(`${i + 1}번째 이미지 업로드 완료`);
     }
-  });
+    await twitterClient.v2.tweet({
+      text: "여친과 살짝 다투었어요 PC카톡 vs 핸드폰카톡",
+      media: {
+        media_ids,
+      },
+    });
+    console.log("done");
+  } catch (e) {
+    console.log(e);
+  }
+  // });
 };
 
 tweet();
