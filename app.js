7c060ae90aede8c6421a9f2ceffd477b65269169
realmojo88 <strikers1999@naver.com>
2022-12-27 16:03:38 +0900

    up

--- app.js
+++ app.js
@@ -50,7 +50,6 @@ const getTrends = async () => {
       "sec-fetch-site": "same-site",
       "user-agent":
         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
-      "x-twitter-auth-type": "OAuth2Session",
     },
   });
   return res.data;
