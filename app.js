9ae9661c649605ddb231fe0b65d61d9a71fe20da
realmojo88 <strikers1999@naver.com>
2023-01-04 12:11:44 +0900

    ㅇ

--- app.js
+++ app.js
@@ -294,6 +294,30 @@ const encodeValue = (text) => {
   );
   return encodedText;
 };
+app.post("/story_upload", async (req, res) => {
+  const { title, textContent } = req.body;
+  let { bo_table } = req.body;
+  if (!bo_table) {
+    bo_table = "worry";
+  }
+
+  // 스토리픽업 포스팅
+  const spFormData = new FormData();
+  spFormData.append("bo_table", bo_table);
+  // spFormData.append("html", "html1");
+  spFormData.append("wr_subject", title);
+  spFormData.append("wr_content", textContent.join("<br/>"));
+
+  const rrr = await axios.post(
+    "https://storypickup.com/bbs/write_auto.php",
+    spFormData
+  );
+
+  let returnUrl = rrr.data;
+
+  res.status(200).send(returnUrl);
+});
+
 app.post("/upload", async (req, res) => {
   const { title, base64image_urls, textContent } = req.body;
   let { bo_table } = req.body;
